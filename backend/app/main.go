7c8fcf0fded1ae3db2ac287ce8c15985630ff7e0
Knpand <f.kntr514@gmail.com>
2022-06-15 18:33:36 +0900

    set CORS OPTIONS #1

--- backend/app/main.go
+++ backend/app/main.go
@@ -34,22 +34,61 @@ var tasks = []Task{{
 }}
 
 
+func handler1(w http.ResponseWriter, r *http.Request) {
+	//CORSの設定
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost")
+	w.Header().Set( "Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS" )
+
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(&tasks); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(buf.String())
+
+	_, err := fmt.Fprint(w, buf.String())
+	if err != nil {
+		return
+	}
+}
+
+
 func main() {
-	handler1 := func(w http.ResponseWriter, r *http.Request) {
+
+	//http.ServeMux:URLにハンドラをバインド
+	router := http.NewServeMux()
+
+	//http.HandlerFunc
+	// ・第１引数にTCPのアドレス
+	// ・第２引数にhttp Handler
+	//　ServeHTTPを実装
+	router.Handle("/task1", http.HandlerFunc(handler1))
+
+	//http.ServeMux.HandlerFunc
+	//HandleFuncのショートカット的な
+	router.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hello World")
 		var buf bytes.Buffer
 		enc := json.NewEncoder(&buf)
 		if err := enc.Encode(&tasks); err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println(buf.String())
-
+	
 		_, err := fmt.Fprint(w, buf.String())
 		if err != nil {
 			return
 		}
-	}
+	})
 
-	// GET /tasks
-	http.HandleFunc("/tasks", handler1)
-	log.Fatal(http.ListenAndServe(":5050", nil))
+
+	
+    //ListenAndServe:サーバの起動
+	// ・第１引数にTCPのアドレス
+	// ・第２引数にhttp Handler
+
+	//log.Fatal:errorになるとexitする
+	log.Fatal(http.ListenAndServe(":5050", router))	
 }
