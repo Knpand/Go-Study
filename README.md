0fb5e04cf036b4a45cb13cc8e08b3055dbae0f64
Knpand <f.kntr514@gmail.com>
2022-06-15 16:35:00 +0900

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Go言語の勉強
+
+## # 環境構築
+
+### ## 導入
+
+以下を順に実行する．
+保険のため、mainブランチへのpushはできないようにする．
+```
+$ git clone https://github.com/Knpand/Go-Study.git
+$ cd Go-Study
+$ docker-compose build
+$ docker-compose up -d
+```
+
+### ## 環境
+
+```
+frontend:Vue.js (port=80)
+backend:Go (port=5050)
+```
+#### 勉強内容ごとにbranch切る
+現状はfrontからbackの5050/taskaにリクエスト飛ばしてもCORSではじかれる
---
+++ frontend/app/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+<h3>Hello world</h3>
+{{response}}
+</template>
+
+<script>
+import axios from 'axios';
+const URL = 'http://127.0.0.1:5050/'
+export default {
+  data(){
+    return{
+      response: "",
+    }
+  },
+  mounted(){
+    axios.get(`${URL}tasks`).then(response =>{
+        
+        this.response = response
+        
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
